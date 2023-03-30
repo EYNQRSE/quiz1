13864a486aed9276337d24fcf0b01f41fdae4af2
AMIR AFZAL <amirwin966@gmail.com>
2023-03-30 16:05:47 +0800

    quiz1.js

---
+++ quiz1.js
@@ -0,0 +1,28 @@
+let str = "1"
+let num = 1.258
+
+let total = parseInt(str) + num
+console.log(total,typeof total)
+
+let data = ["benr", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+
+let user = {
+    name: "amir",
+    age: 30,
+    faculty: "fkekk",
+}
+
+console.log(user.name)
+console.log(user.age)
+
+let message = [
+    {name:"usr1",date:"2/4/2023", msg:"very good video",like: 1000},
+    {name:"usr2",date:"1/4/2023", msg:"wow so sexy",like: 9000},
+    {name:"usr3",date:"3/4/2023", msg:"keep it going",like: 30}
+]
+
+console.log(message[1].msg)
